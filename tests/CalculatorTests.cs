fd9eea814257a8ca9d3b8be7e9c1fa785f306496
Tomáš Dračka <tomidracka@gmail.com>
2024-02-25 23:41:24 +0100

    More CalculatorTests

--- tests/CalculatorTests.cs
+++ tests/CalculatorTests.cs
@@ -45,4 +45,24 @@ public class CalculatorTests
     {
         Assert.Throws<System.DivideByZeroException>(() => _calculator.Divide(5, 0));
     }
+    
+    [Test]
+    public void AddNegativeNumbersTest()
+    {
+        var result = _calculator.Add(-5, -3);
+        Assert.That(result, Is.EqualTo(-8));
+    }
+
+    [Test]
+    public void DivideZeroByZeroTest()
+    {
+        Assert.Throws<System.DivideByZeroException>(() => _calculator.Divide(0, 0));
+    }
+    
+    [Test]
+    public void MultiplyLargeNumbersTest()
+    {
+        var result = _calculator.Multiply(1e300, 2);
+        Assert.That(result, Is.EqualTo(2e300));
+    }
 }
