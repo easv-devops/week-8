7e20a79fe818337c208d0975a8fac40bf99aed9d
Tomáš Dračka <tomidracka@gmail.com>
2024-02-25 23:55:40 +0100

    Database Integration test

--- application/Database.cs
+++ application/Database.cs
@@ -30,4 +30,29 @@ public class Database
             await conn.ExecuteAsync(sql, parameters);
         }
     }
+    
+    public async Task<CalculationLogEntry> GetLogEntry(string operation, double n1, double n2, double result)
+    {
+        using (var conn = new NpgsqlConnection(_connString))
+        {
+            const string sql = @"
+                    SELECT operation, number1, number2, result
+                    FROM calculations
+                    WHERE operation = @Operation AND number1 = @Number1 AND number2 = @Number2 AND result = @Result
+                    LIMIT 1;
+                ";
+
+            var parameters = new { Operation = operation, Number1 = n1, Number2 = n2, Result = result };
+            var logEntry = await conn.QueryFirstOrDefaultAsync<CalculationLogEntry>(sql, parameters);
+            return logEntry;
+        }
+    }
+}
+
+public class CalculationLogEntry
+{
+    public string Operation { get; set; }
+    public double Number1 { get; set; }
+    public double Number2 { get; set; }
+    public double Result { get; set; }
 }
