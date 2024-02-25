0720d062cea6767f4e88f2d3eee7018141709c3d
Tomáš Dračka <tomidracka@gmail.com>
2024-02-26 00:00:45 +0100

    Fix

--- application/Database.cs
+++ application/Database.cs
@@ -10,10 +10,11 @@ public class Database
     public Database()
     {
         _connString = Environment.GetEnvironmentVariable("ConnectionString")!;
-        
+
         if (string.IsNullOrEmpty(_connString))
         {
-            throw new InvalidOperationException("The connection string environment variable 'ConnectionString' is not set.");
+            throw new InvalidOperationException(
+                "The connection string environment variable 'ConnectionString' is not set.");
         }
     }
 
@@ -30,29 +31,4 @@ public class Database
             await conn.ExecuteAsync(sql, parameters);
         }
     }
-    
-    public async Task<CalculationLogEntry> GetLogEntry(string operation, double n1, double n2, double result)
-    {
-        using (var conn = new NpgsqlConnection(_connString))
-        {
-            const string sql = @"
-                    SELECT operation, number1, number2, result
-                    FROM calculations
-                    WHERE operation = @Operation AND number1 = @Number1 AND number2 = @Number2 AND result = @Result
-                    LIMIT 1;
-                ";
-
-            var parameters = new { Operation = operation, Number1 = n1, Number2 = n2, Result = result };
-            var logEntry = await conn.QueryFirstOrDefaultAsync<CalculationLogEntry>(sql, parameters);
-            return logEntry;
-        }
-    }
-}
-
-public class CalculationLogEntry
-{
-    public string Operation { get; set; }
-    public double Number1 { get; set; }
-    public double Number2 { get; set; }
-    public double Result { get; set; }
 }
